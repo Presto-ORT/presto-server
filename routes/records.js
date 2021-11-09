01e6241371ddb4188550177f0888cbdd8296e72d
dtlucho <luciano.ditomaso@gmail.com>
2021-11-09 20:12:20 -0300

    [FT] Adding records main endpoints and comments on main logic

--- routes/records.js
+++ routes/records.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.send('respond with all records');
 });
+
+router.post('/', function (req, res, next) {
+    // recibo importe, categoria, subcat, fecha, descripcion
+    res.send('Create a record');
+});
+
+router.get('/:id', function (req, res, next) {
+    // recibo id
+    // si encuentro gasto, lo mando
+    // sino, respondo error
+    res.send('Get a record');
+});
+
+function authorization(req, res, next) {
+    // validar token de usuario
+    // si no esta ok, dar error
+    // sino guardar user en request
+    next();
+}
 
 module.exports = router;
