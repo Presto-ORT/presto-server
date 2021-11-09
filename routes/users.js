37dc1153073f207833f57e15db7e516551762761
dtlucho <luciano.ditomaso@gmail.com>
2021-11-09 19:49:29 -0300

    [FT] Adding endpoints with comments of main logic

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,20 @@ router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/register', function (req, res, next) {
+  // recibimos nombre, mail y contraseña
+  // validamos que la contraseña no exista
+  // si no existe, creamos el usuario y creamos token
+  // si existe, respondemos error
+  res.status(201).json({});
+});
+
+router.get('/login', function (req, res, next) {
+  // recibimos mail y contraseña
+  // validamos contraseña
+  // si esta todo ok, generamos token
+  // sino, respondemos error
+  res.json({});
+});
+
 module.exports = router;
