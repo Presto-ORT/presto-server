16e383f1d07dd865699dc1f5d53578ee7dfee60d
Leo Fernandez <31252634@comunidadort.edu.ar>
2021-11-09 22:04:03 -0300

    CRUD Inicial

---
+++ data/categories.js
@@ -0,0 +1,27 @@
+const { ObjectId } = require('bson');
+const conn = require('./connection.js');
+const DATABASE = 'prestoDB';
+const CATEGORIES = 'categories';
+
+
+async function getAllCategories(){
+    const connectiondb = await conn.getConnection();
+    const categories = await connectiondb
+                        .db(DATABASE)
+                        .collection(CATEGORIES)
+                        .find()
+                        .toArray();    
+    return categories;
+}
+
+async function getCategoryById(id){
+    const connectiondb = await conn.getConnection();
+    const category = await connectiondb
+                        .db(DATABASE)
+                        .collection(CATEGORIES)
+                        .findOne({_id: new ObjectId(id)})
+                        
+    return category;
+}
+
+module.exports = {getAllCategories, getCategoryById};
---
+++ data/records.js
@@ -0,0 +1,50 @@
+const { ObjectId } = require('bson');
+const conn = require('./connection.js');
+const DATABASE = 'prestoDB';
+const CATEGORIES = 'records';
+
+
+async function getAllRecords(){
+    const connectiondb = await conn.getConnection();
+    const records = await connectiondb
+                        .db(DATABASE)
+                        .collection(CATEGORIES)
+                        .find()
+                        .toArray();    
+    return records;
+}
+
+async function getRecordById(id){
+    const connectiondb = await conn.getConnection();
+    const record = await connectiondb
+                        .db(DATABASE)
+                        .collection(CATEGORIES)
+                        .findOne({_id: new ObjectId(id)})
+                        
+    return record;
+}
+
+async function addNewRecord(newRecord){
+    const connectiondb = await conn.getConnection();
+    
+    const record = await connectiondb
+                        .db(DATABASE)
+                        .collection(CATEGORIES)
+                        .insertOne(newRecord)
+                        
+    return record;
+}
+
+async function deleteRecord(id){
+    const connectiondb = await conn.getConnection();
+    
+    const result = await connectiondb
+                        .db(DATABASE)
+                        .collection(CATEGORIES)
+                        .deleteOne({_id: new ObjectId(id)})
+                        
+    return result;
+}
+
+
+module.exports = {getAllRecords, getRecordById, addNewRecord};
