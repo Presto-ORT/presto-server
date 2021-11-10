5eaf32a2c4e1874f3db7a4a55377a3c3cf5fba5c
dtlucho <luciano.ditomaso@gmail.com>
2021-11-09 22:35:00 -0300

    [FT] Main logic in endpoints

--- routes/categories.js
+++ routes/categories.js
@@ -1,13 +1,23 @@
 const express = require('express');
-const { BandwidthLimitExceeded } = require('http-errors');
 const router = express.Router();
 
-router.get('/', function (req, res, next) {
-    res.send('respond with a resource');
+const categoriesDB = require('../data/categories');
+
+router.get('/', async function (req, res, next) {
+    const categories = await categoriesDB.getAllCategories();
+    if (!categories.length === 0) return res.status(404).json({ error: "No encontrado", description: "No se encontro ninguna categoria" });
+
+    res.json(categories);
 });
 
-router.get('/:id', function (req, res, next) {
-    res.send('respond with a resource');
+router.get('/:id', async function (req, res, next) {
+    const { id } = req.params;
+    if (!id) return res.status(400).json({ error: "Parametro faltante", description: "No se puede obtener una categoria si no se proporciona id" });
+
+    const category = await categoriesDB.getCategoryById(id);
+    if (!category) return res.status(404).json({ error: "No encontrado", description: "No se encontro ninguna categoria con el id proporcionado" });
+
+    res.json(category);
 });
 
 module.exports = router;
