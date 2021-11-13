1c7c50a6fda23ab59a949d32d8335a36228ceb3b
dtlucho <luciano.ditomaso@gmail.com>
2021-11-13 01:05:49 -0300

    [WHITESPACES]

--- data/records.js
+++ data/records.js
@@ -1,50 +1,50 @@
 const { ObjectId } = require('bson');
 const conn = require('./connection.js');
 const DATABASE = 'prestoDB';
-const CATEGORIES = 'records';
+const RECORDS = 'records';
 
 
-async function getAllRecords(){
+async function getAllRecords() {
     const connectiondb = await conn.getConnection();
     const records = await connectiondb
-                        .db(DATABASE)
-                        .collection(CATEGORIES)
-                        .find()
-                        .toArray();    
+        .db(DATABASE)
+        .collection(RECORDS)
+        .find()
+        .toArray();
     return records;
 }
 
-async function getRecordById(id){
+async function getRecordById(id) {
     const connectiondb = await conn.getConnection();
     const record = await connectiondb
-                        .db(DATABASE)
-                        .collection(CATEGORIES)
-                        .findOne({_id: new ObjectId(id)})
-                        
+        .db(DATABASE)
+        .collection(RECORDS)
+        .findOne({ _id: new ObjectId(id) })
+
     return record;
 }
 
-async function addNewRecord(newRecord){
+async function addNewRecord(newRecord) {
     const connectiondb = await conn.getConnection();
-    
+
     const record = await connectiondb
-                        .db(DATABASE)
-                        .collection(CATEGORIES)
-                        .insertOne(newRecord)
-                        
+        .db(DATABASE)
+        .collection(RECORDS)
+        .insertOne(newRecord)
+
     return record;
 }
 
-async function deleteRecord(id){
+async function deleteRecord(id) {
     const connectiondb = await conn.getConnection();
-    
+
     const result = await connectiondb
-                        .db(DATABASE)
-                        .collection(CATEGORIES)
-                        .deleteOne({_id: new ObjectId(id)})
-                        
+        .db(DATABASE)
+        .collection(RECORDS)
+        .deleteOne({ _id: new ObjectId(id) })
+
     return result;
 }
 
 
-module.exports = {getAllRecords, getRecordById, addNewRecord};
+module.exports = { getAllRecords, getRecordById, addNewRecord };
