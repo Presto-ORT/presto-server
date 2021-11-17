7b6284271bed30af66a8a6fb1250d24150d9099e
dtlucho <luciano.ditomaso@gmail.com>
2021-11-17 18:49:27 -0300

    [FT] Adding reports router to main app

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const recordsRouter = require('./routes/records');
 const categoriesRouter = require('./routes/categories');
+const reportsRouter = require('./routes/reports');
 
 const app = express();
 
 // view engine setup
-app.options("*", cors({ origin: 'http://localhosdt:19006', optionsSuccessStatus: 200}));
-app.use(cors({ origin: 'http://localhost:19002', optionsSuccessStatus: 200}));
+app.options("*", cors({ origin: 'http://localhosdt:19006', optionsSuccessStatus: 200 }));
+app.use(cors({ origin: 'http://localhost:19002', optionsSuccessStatus: 200 }));
 /// CORS
 
 app.set('views', path.join(__dirname, 'views'));
@@ -30,6 +31,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/records', recordsRouter);
 app.use('/categories', categoriesRouter);
+app.use('/reports', reportsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
